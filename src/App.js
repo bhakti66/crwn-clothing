b393cde8ea5ddd4e8499781ff90a6b7df2d2f510
Bhakti <blgandhi66@gmail.com>
2021-01-21 22:56:36 +0530

    Sign in page with custom input and button components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
-
+import SignInUpPage from './pages/sign-in-up/sign-in-up.component'
 const HatsPage=()=>(
   <div>
     <h1>HATS PAGE</h1>
@@ -18,6 +18,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route path="/shop" component={ShopPage}></Route>
+        <Route path="/signin" component={SignInUpPage}></Route>
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-up/sign-in-up.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './sign-in-up.styles.scss'
+import SignIn from '../../components/sign-in/sign-in.component'
+const SignInUpPage = () => (
+    <div>
+        <SignIn />
+    </div>
+)
+
+export default SignInUpPage;
